8f7e4d2227bccc078096e913c2c4462d7242afda
Kate <kattycandy@mail.ru>
2019-06-25 12:07:42 +0600

    created a file

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,28 @@
 package com.mycompany.app;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.io.FileWriter;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public App() {
     }
+
+    public static void main(String[] args ) throws IOException {
+        ArrayList<String> strings = new ArrayList<String>();
+        strings.add("string1");
+        strings.add("string2");
+        strings.add("string3");
+        FileWriter writer = new FileWriter("output.txt");
+        for(String str: strings) {
+            writer.write(str);
+        }
+        writer.close();
+    }
+
 }
