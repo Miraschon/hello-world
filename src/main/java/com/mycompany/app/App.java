6da88e1e3135c9be999944d54cac550a69e628be
Kate <kattycandy@mail.ru>
2019-06-26 10:50:45 +0600

    major adjustments

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,8 +1,12 @@
 package com.mycompany.app;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.io.FileWriter;
+import java.util.List;
 
 /**
  * Hello world!
@@ -10,16 +14,28 @@ import java.io.FileWriter;
  */
 public class App {
 
-    public static void main(String[] args ) throws IOException {
+    private static final Logger log = LogManager.getLogger(App.class);
+
+    void writeToFile(List<String> strings){
+        log.debug("start");
+        try (PrintWriter writer = new PrintWriter("target/output.txt")) {
+            for (String str : strings) {
+                writer.println(str);
+            }
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+        log.debug("finish");
+    }
+
+    public static void main(String[] args ) {
         ArrayList<String> strings = new ArrayList<>();
         strings.add("string1");
         strings.add("string2");
         strings.add("string3");
-        FileWriter writer = new FileWriter("output.txt");
-        for(String str: strings) {
-            writer.write(str);
-        }
-        writer.close();
+
+        App app = new App();
+        app.writeToFile(strings);
     }
 
 }
