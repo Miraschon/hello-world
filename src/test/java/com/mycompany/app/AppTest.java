755bddee1b52dac5a37c8a7c472b429b465a0422
Kate <kattycandy@mail.ru>
2019-06-26 10:47:39 +0600

    major changes, test

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -2,6 +2,13 @@ package com.mycompany.app;
 
 import org.junit.Test;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 
@@ -12,7 +19,31 @@ public class AppTest  {
 
     @Test
     public void myTest() {
-        boolean a = true;
-        assertTrue(a);
+        List<String> expected = new ArrayList<>();
+        expected.add("string1");
+        expected.add("string2");
+        expected.add("string3");
+
+        App app = new App();
+        app.writeToFile(expected);
+
+        List<String> test = new ArrayList<>();
+        // читаем файл построчно в test
+        try (BufferedReader br = new BufferedReader(new FileReader("target/output.txt")))
+        {
+
+            String sCurrentLine;
+
+            while ((sCurrentLine = br.readLine()) != null) {
+                test.add(sCurrentLine);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // сравниваем два списка
+
+        assertEquals(expected,test);
     }
 }
